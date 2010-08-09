ccf9a19ab856186a85687d5f046d831f628ed10b
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-08-09 03:01:31 +0000

    Negro:

---
+++ trunk/ProyectoHotel/src/java/Intermediarios/IntermediarioBean.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Intermediarios;
+
+/**
+ *
+ * @author Manuel
+ */
+class IntermediarioBean<T> {
+
+}
---
+++ trunk/ProyectoHotel/src/java/Intermediarios/IntermediarioTramo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Intermediarios;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Manuel
+ */
+@Remote
+public interface IntermediarioTramo<E> extends Intermediario<E> {
+    
+}
