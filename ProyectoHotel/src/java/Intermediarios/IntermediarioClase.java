10149222fc0aa8895e4ee889860a947631c2aa54
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-09-14 11:44:03 +0000

    Negro:

---
+++ ProyectoHotel/src/java/Intermediarios/IntermediarioClase.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Intermediarios;
+
+import Entidades.Clase;
+import java.util.List;
+
+/**
+ *
+ * @author Manuel
+ */
+public class IntermediarioClase extends Intermediario<Clase>{
+
+    @Override
+    public List<Clase> findByDto(Object dto) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
