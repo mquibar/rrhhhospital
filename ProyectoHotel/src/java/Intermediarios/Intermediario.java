3843548986050b5b563b32b93cd65c8eea58898d
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-10-08 22:55:04 +0000

    Negro: --> 08/10 ==> Intermediarios listos para findByDto

--- ProyectoHotel/src/java/Intermediarios/Intermediario.java
+++ ProyectoHotel/src/java/Intermediarios/Intermediario.java
@@ -2,40 +2,40 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Intermediarios;
 
 import Configuraciones.LogAdmin;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.Query;
 import org.apache.commons.logging.Log;
-
 
 /**
  *
  * @author desarrollo
  */
-public abstract class Intermediario<E>{
+public abstract class Intermediario<E> {
 
-    protected Log _log= LogAdmin.getInstance().getLog(this.getClass());
+    protected Log _log = LogAdmin.getInstance().getLog(this.getClass());
     protected String _clase;
 
     public List<E> findAll() {
-        try{
+        try {
             Query q = GestorConeccion.getInstance().getManager().createNamedQuery(_clase + ".findAll");
             return q.getResultList();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             _log.error(ex.getMessage());
             return null;
         }
     }
 
-    public boolean guardar(E obj){
-        try{
+    public boolean guardar(E obj) {
+        try {
             GestorConeccion.getInstance().getManager().persist(obj);
             return true;
-        }catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println("************* <Error al guardar>");
             ex.printStackTrace();
             _log.error(ex.getMessage());
@@ -44,11 +44,11 @@ public abstract class Intermediario<E>{
         }
     }
 
-    public boolean actualizar(E obj){
-        try{
+    public boolean actualizar(E obj) {
+        try {
             GestorConeccion.getInstance().getManager().merge(obj);
             return true;
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             _log.error(ex.getMessage());
             return false;
@@ -56,6 +56,32 @@ public abstract class Intermediario<E>{
     }
 
     public abstract List<E> findInOrden(String orden);
-    
+
     public abstract List<E> findByDto(Object dto);
+
+    protected Query crearQuery(Map<String, Object> restricciones) {
+        String sql = "SELECT o FROM " + _clase + " o";
+        
+        if (restricciones.isEmpty()) {
+            return GestorConeccion.getInstance().getManager().createQuery(sql);
+        }
+        Query q = null;
+        sql += "WHERE ";
+        Iterator<String> i = restricciones.keySet().iterator();
+        while (i.hasNext()) {
+            String key = i.next();
+            sql += "o." + key + "= :" + key + " AND ";
+        }
+        sql = sql.substring(0, 5);
+        System.out.println(sql);
+
+        q = GestorConeccion.getInstance().getManager().createQuery(sql);
+
+        i = restricciones.keySet().iterator();
+        while (i.hasNext()) {
+            String key = i.next();
+            q.setParameter(key, restricciones.get(key));
+        }
+        return q;
+    }
 }
