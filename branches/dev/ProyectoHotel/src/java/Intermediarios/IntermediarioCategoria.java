1d74458cc742555ad6e2e4bf013316f9dec76b50
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-10-08 22:55:04 +0000

    Negro: --> 08/10 ==> Intermediarios listos para findByDto

--- branches/dev/ProyectoHotel/src/java/Intermediarios/IntermediarioCategoria.java
+++ branches/dev/ProyectoHotel/src/java/Intermediarios/IntermediarioCategoria.java
@@ -2,41 +2,43 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Intermediarios;
 
 import Configuraciones.LogAdmin;
 import DTO.DtoCategoria;
 import Entidades.Categoria;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author Familia
  */
-
 public class IntermediarioCategoria extends Intermediario<Categoria> {
 
     public IntermediarioCategoria() {
-        _clase="Categoria";
+        _clase = "Categoria";
         _log = LogAdmin.getInstance().getLog(this.getClass());
     }
-
 
     @Override
     public List<Categoria> findByDto(Object dto) {
         DtoCategoria dtoCat = (DtoCategoria) dto;
+        Map<String, Object> restricciones = new HashMap<String, Object>();
+        
+        if (dtoCat.getNombre() != null) {
+            restricciones.put("nombre", dtoCat.getNombre());
+        }
+        if (dtoCat.getTramo() != null) {
+            restricciones.put("idTramo", dtoCat.getTramo());
+        }
         try {
-            Criteria criteria = ((Session) GestorConeccion.getInstance().getManager().getDelegate()).createCriteria(Categoria.class);
-            criteria.add(Restrictions.eq("nombre", dtoCat.getNombre()));
-            return criteria.list();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            _log.error(ex.getMessage());
+            return crearQuery(restricciones).getResultList();
+        } catch (Exception e) {
             return null;
         }
     }
@@ -46,5 +48,4 @@ public class IntermediarioCategoria extends Intermediario<Categoria> {
         Criteria criterio = ((Session) GestorConeccion.getInstance().getManager().getDelegate()).createCriteria(Categoria.class).addOrder(Order.asc(orden));
         return criterio.list();
     }
-
 }
