e5e4fc66956e24b983010b7621961f5eeebc24be
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-10-11 05:13:07 +0000

    Negro: --> 10/10 ==>

--- ProyectoHotel/src/java/Intermediarios/GestorConeccion.java
+++ ProyectoHotel/src/java/Intermediarios/GestorConeccion.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Intermediarios;
 
 import Configuraciones.loadConfig;
@@ -19,86 +18,92 @@ public class GestorConeccion {
 
     //@PersistenceUnit
     private EntityManagerFactory _factory;
-    private EntityManager _manager=null;
-    private int _contador=0;
-    private static GestorConeccion _instance=null;
+    private EntityManager _manager = null;
+    private int _contador = 0;
+    private static GestorConeccion _instance = null;
     private Log _log;
 
-    public static GestorConeccion getInstance(){
-        if(_instance == null)
+    public static GestorConeccion getInstance() {
+        if (_instance == null) {
             _instance = new GestorConeccion();
+        }
         return _instance;
     }
 
-    public GestorConeccion(){
+    public GestorConeccion() {
         loadConfig.configurarConexion();
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         System.out.println("Conexion = " + System.getProperty("hibernate.connection.url"));
         System.out.println("Usuario = " + System.getProperty("hibernate.connection.username"));
         System.out.println("Pass = " + System.getProperty("hibernate.connection.password"));
-        System.out.println("JNDI = " + System.getProperty("post-gre-sql_hospitaltest_postgresPool.Password"));
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        _factory = Persistence.createEntityManagerFactory("ProyectoHotelPU",System.getProperties());
+
+        _factory = Persistence.createEntityManagerFactory("ProyectoHotelPU", System.getProperties());
         _log = Configuraciones.LogAdmin.getInstance().getLog(this.toString());
-        
+
     }
 
     public void beginTransaction() {
-        if(_manager != null )
-            if(_manager.isOpen() && _contador>0 && _manager.getTransaction().isActive()){
+        if (_manager != null) {
+            if (_manager.isOpen() && _contador > 0 && _manager.getTransaction().isActive()) {
                 _contador++;
                 return;
+            }
+        } else {
+            getManager();
         }
-        try{
-            if(!_manager.isOpen())
-                _manager=_factory.createEntityManager();
+
+        try {
+            if (!_manager.isOpen()) {
+                _manager = _factory.createEntityManager();
+            }
             _manager.getTransaction().begin();
             _contador++;
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
             _log.error(ex.getMessage());
         }
     }
 
     public void commitTransaction() throws Exception {
-        if(_contador>1){
+        if (_contador > 1) {
             _contador--;
             return;
         }
-        if(_manager.isOpen()&& _manager.getTransaction().isActive()){
-            try{
+        if (_manager.isOpen() && _manager.getTransaction().isActive()) {
+            try {
                 _manager.getTransaction().commit();
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace();
                 _log.error(ex.getMessage());
                 throw new Exception(ex);
-            }finally{
+            } finally {
                 _contador = 0;
             }
         }
     }
 
     public void rollbackTransaction() {
-        if(_contador>1){
+        if (_contador > 1) {
             _contador--;
             return;
         }
-        if(_manager.isOpen()){
-            try{
+        if (_manager.isOpen()) {
+            try {
                 _manager.getTransaction().rollback();
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 ex.printStackTrace();
                 _log.error(ex.getMessage());
-            }finally{
+            } finally {
                 _contador = 0;
             }
         }
     }
 
-    public EntityManager getManager(){
-        if(_manager == null)
+    public EntityManager getManager() {
+        if (_manager == null) {
             _manager = _factory.createEntityManager();
+        }
         return _manager;
     }
-
 }
--- ProyectoHotel/src/java/Intermediarios/IntermediarioClase.java
+++ ProyectoHotel/src/java/Intermediarios/IntermediarioClase.java
@@ -5,8 +5,11 @@
 
 package Intermediarios;
 
+import DTO.DtoClase;
 import Entidades.Clase;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
@@ -17,9 +20,18 @@ import org.hibernate.criterion.Order;
  */
 public class IntermediarioClase extends Intermediario<Clase>{
 
+    public IntermediarioClase() {
+        _clase = "Clase";
+    }
+
+
     @Override
     public List<Clase> findByDto(Object dto) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        DtoClase dtoClase = (DtoClase)dto;
+        Map<String,Object> restricciones = new HashMap<String, Object>();
+        if(dtoClase.getNombre()!=null)
+            restricciones.put("nombre", dtoClase.getNombre());
+        return crearQuery(restricciones).getResultList();
     }
 
     @Override
