88e02c33e4cac03c4fecc86cab68efeeebe80bcb
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-11-03 22:54:57 +0000

    Negro: --> 03/11 ==> Esto Anda con AJAX

---
+++ src/java/servlets/testController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servlets;
+
+import Expertos.ExpModificarAgrupamiento;
+import controllers.GeneralController;
+import models.combos.AbstractModelOptionList;
+import models.combos.ModelOptionAgrupamiento;
+import models.combos.ModelOptionTramo;
+
+/**
+ *
+ * @author Manuel
+ */
+public class testController extends GeneralController {
+
+    ExpModificarAgrupamiento _exp;
+    ModelOptionAgrupamiento agrup=null;
+    public testController() {
+        _exp = (ExpModificarAgrupamiento) super.getExpert(ExpModificarAgrupamiento.class.getName());
+    }
+
+    public AbstractModelOptionList getAgrupamientos(){
+        if(agrup ==null)
+            agrup = new ModelOptionAgrupamiento(_exp.inicioModificacion());
+        return agrup;
+    }
+
+    public AbstractModelOptionList getTramo(String value){
+        if(agrup == null) getAgrupamientos();
+        try{
+            Thread.sleep(2000);
+            return new ModelOptionTramo(_exp.consultarTramos(agrup.getSelectedItem(Integer.valueOf(value))));
+        }catch(Exception ex){
+            return new ModelOptionTramo(null);
+        }
+    }
+
+}
