dcbe81f3f04efd88325f4c3cf6fb901a600cc521
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-12-01 06:10:58 +0000

    Negro: -->

--- branches/ManagerRRHH-Hospital/src/java/seguimiento/gestionCategoria/ctrlAltaCategoria.java
+++ branches/ManagerRRHH-Hospital/src/java/seguimiento/gestionCategoria/ctrlAltaCategoria.java
@@ -9,6 +9,8 @@ import Entidades.Clase;
 import Entidades.Requisito;
 import Entidades.Tramo;
 import Expertos.categorizacion.ExpAltaCategoria;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import managerrrhhhospital.ContextGenerator;
 import models.tables.TableClase;
@@ -23,15 +25,66 @@ public class ctrlAltaCategoria {
     private AltaCategoria _pantalla;
     private ExpAltaCategoria _gestorCategoria;
     private TableRequisitos _requisitos;
-    private TableClase _clases, clasesContenidas;
+    private TableClase _clases, _clasesContenidas;
 
     public ctrlAltaCategoria(AdminEscalafon pantalla, Tramo tramo) {
         _pantalla = new AltaCategoria(pantalla, this);
         _gestorCategoria = (ExpAltaCategoria) ContextGenerator.getInstance().createGestor(ExpAltaCategoria.class.getName());
         _clases = new TableClase(_gestorCategoria.iniciarCU(tramo));
-        clasesContenidas = new TableClase(new ArrayList<Clase>());
+        _clasesContenidas = new TableClase(new ArrayList<Clase>());
         _requisitos = new TableRequisitos(new ArrayList<Requisito>());
 
+        _pantalla.getBtnAddRequisito().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressAddRequest();
+            }
+        });
+        _pantalla.getBtnDelRequisito().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressRemoveRequest();
+            }
+        });
+
+        _pantalla.getBtnAddOne().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressAddOne();
+            }
+        });
+        _pantalla.getBtnAddAll().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressAddAll();
+            }
+        });
+
+        _pantalla.getBtnRemoveOne().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressRemoveOne();
+            }
+        });
+        _pantalla.getBtnRemoveAll().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressRemoveAll();
+            }
+        });
+
+        _pantalla.getBtnCancel().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressCancelButton();
+            }
+        });
+        _pantalla.getBtnOk().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressOkButton();
+            }
+        });
     }
 
     void pressCancelButton(){
@@ -41,4 +94,63 @@ public class ctrlAltaCategoria {
     void pressOkButton(){
         
     }
+
+    void pressAddRequest(){
+        Requisito r = new Requisito();
+        r.setDescripcion("Nuevo Requisito");
+        r.setNumero(_requisitos.getRowCount());
+        _requisitos.addRow(r);
+        _requisitos.sort();
+        _pantalla.getTblRequisitos().editCellAt(_requisitos.getRowCount()-1, 1);
+    }
+
+    void pressRemoveRequest(){
+        _requisitos.delRow(_pantalla.getTblRequisitos().getSelectedRow());
+        _requisitos.sort();
+    }
+
+    void pressAddOne(){
+        _clasesContenidas.addRow(_clases.getSelectedIndex(_pantalla.getTblClases().getSelectedRow()));
+        _clases.delRow(_pantalla.getTblClases().getSelectedRow());
+        _clases.sort();
+        _clasesContenidas.sort();
+        boolean enabled = _clases.getRowCount()>0;
+        _pantalla.getBtnAddAll().setEnabled(enabled);
+        _pantalla.getBtnAddOne().setEnabled(enabled);
+        _pantalla.getBtnRemoveOne().setEnabled(true);
+        _pantalla.getBtnRemoveAll().setEnabled(true);
+    }
+    void pressAddAll(){
+        _clasesContenidas.addAll(_clases.getAllRow());
+        _clases.clear();
+        _clasesContenidas.sort();
+        _pantalla.getBtnAddAll().setEnabled(false);
+        _pantalla.getBtnAddOne().setEnabled(false);
+        _pantalla.getBtnRemoveOne().setEnabled(true);
+        _pantalla.getBtnRemoveAll().setEnabled(true);
+    }
+
+    void pressRemoveOne(){
+        _clases.addRow(_clasesContenidas.getSelectedIndex(_pantalla.getTblClasesAsignadas().getSelectedRow()));
+        _clasesContenidas.delRow(_pantalla.getTblClasesAsignadas().getSelectedRow());
+        _clasesContenidas.sort();
+        _clases.sort();
+        boolean enabled = _clasesContenidas.getRowCount()>0;
+        _pantalla.getBtnAddAll().setEnabled(true);
+        _pantalla.getBtnAddOne().setEnabled(true);
+        _pantalla.getBtnRemoveOne().setEnabled(enabled);
+        _pantalla.getBtnRemoveAll().setEnabled(enabled);
+    }
+
+    void pressRemoveAll(){
+        _clases.addAll(_clasesContenidas.getAllRow());
+        _clasesContenidas.clear();
+        _clases.sort();
+        _pantalla.getBtnRemoveOne().setEnabled(false);
+        _pantalla.getBtnRemoveAll().setEnabled(false);
+        _pantalla.getBtnAddAll().setEnabled(true);
+        _pantalla.getBtnAddOne().setEnabled(true);
+    }
 }
+
+
