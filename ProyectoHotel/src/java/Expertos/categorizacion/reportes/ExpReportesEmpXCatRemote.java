d0d2106202821d3e13e1fd76df843ab0d0c026df
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-12-06 16:38:10 +0000

    Negro: --->

---
+++ ProyectoHotel/src/java/Expertos/categorizacion/reportes/ExpReportesEmpXCatRemote.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Expertos.categorizacion.reportes;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author desarrollo
+ */
+@Remote
+public interface ExpReportesEmpXCatRemote {
+
+    public java.util.List<Entidades.Agrupamiento> listarAgrupamiento();
+
+    public java.util.List<Entidades.Tramo> listarTramo(Entidades.Agrupamiento a);
+
+    public java.util.List<Entidades.Categoria> listarCategorias(Entidades.Tramo t);
+
+    public net.sf.jasperreports.engine.JasperPrint generarReporte();
+
+    public net.sf.jasperreports.engine.JasperPrint generarReporte(Entidades.Categoria c);
+    
+}
