d62780db8475dc2dd0071959bd9be3e2260dedd5
negromanuel.quibar <negromanuel.quibar@29706a64-f129-2db0-2637-2e5b7570b2ea>
2010-12-06 16:45:31 +0000

    Negro: --->

---
+++ branches/Cliente/src/java/controllers/ctrlReporteEmpCat.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controllers;
+
+import Entidades.Categoria;
+import Expertos.categorizacion.reportes.ExpReportesEmpXCatRemote;
+import models.combos.AbstractModelOptionList;
+import models.combos.ModelOptionAgrupamiento;
+import models.combos.ModelOptionCategoria;
+import models.combos.ModelOptionTramo;
+import net.sf.jasperreports.view.JasperViewer;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class ctrlReporteEmpCat extends GeneralController {
+
+    private ExpReportesEmpXCatRemote _exp;
+    private ModelOptionAgrupamiento _modAgrupamiento;
+    private ModelOptionTramo _modTramo;
+    private ModelOptionCategoria _modCategoria;
+
+    public ctrlReporteEmpCat() {
+        _exp = (ExpReportesEmpXCatRemote) super.getExpert(ExpReportesEmpXCatRemote.class.getName());
+    }
+
+    public AbstractModelOptionList listarAgrupamientos() {
+        _modAgrupamiento = new ModelOptionAgrupamiento(_exp.listarAgrupamiento());
+        return _modAgrupamiento;
+    }
+
+    public AbstractModelOptionList listarTramo(String Index) {
+        _modTramo = new ModelOptionTramo(_exp.listarTramo(_modAgrupamiento.getSelectedItem(Index)));
+        return _modTramo;
+    }
+
+    public AbstractModelOptionList listarCategorias(String IndexTramo) {
+        _modCategoria = new ModelOptionCategoria(_exp.listarCategorias(_modTramo.getSelectedItem(IndexTramo)));
+        return _modCategoria;
+    }
+
+    public byte[] armarReporte(){
+        return (new ctrlReporte()).armarReporte(_exp.generarReporte());
+    }
+
+    public byte[] armarReporte(Categoria c){
+        return (new ctrlReporte()).armarReporte(_exp.generarReporte(c));
+    }
+
+    public void verReportes(){
+        JasperViewer.viewReport(_exp.generarReporte());
+    }
+
+    public void verReportes(String IndexCategoria){
+        Categoria c = _modCategoria.getSelectedItem(IndexCategoria);
+        if(c== null) return;
+        JasperViewer.viewReport(_exp.generarReporte(c));
+    }
+
+}
